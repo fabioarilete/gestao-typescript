0b25080db3d4720a5c80a38eade5820fdf848ace
fabioarilete <fabioarilete@hotmail.com>
2023-10-11 21:50:11 -0300

    feat:files configuration

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Header = styled.div`
+  background-color: black;
+  height: 100px;
+`;
+
+export const Body = styled.div`
+  background-color: red;
+  color: yellow;
+  font-size: 50px;
+  margin: auto;
+  max-width: 980px;
+  margin-bottom: 50px;
+`;
+
+export const Footer = styled.div`
+  background-color: black;
+  color: #fff;
+  padding: 3em;
+  text-align: center;
+`;
